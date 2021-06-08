0cd50137d431b63d48dd88a49bd62c2f291784ae
Gerardo Mangila <glitch501@yahoo.com>
2021-06-08 18:44:12 -0500

    Update Spam.py

--- Spam.py
+++ Spam.py
@@ -6,13 +6,20 @@ imap_url = 'imap.aol.com'
 
 con = imaplib.IMAP4_SSL(imap_url) #SSL connection
 con.login(user, password)
-con.select("Bulk Mail") #Spam Folder
+con.select('"Bulk Mail"') #name of folder Ex. Inbox
 
-typ, msg = con.search(None, 'All')
-print(msg[0])
-for num in msg[0].split():
-        con.store(num, '+FLAGS', '\\Deleted $Junk')
-        
-
+typ, msg = con.search(None, 'All') #search all inbox
+print(msg)
+Email = msg[0].split()
+print (Email)
+while Email:
+        for num in Email:
+                try:
+                        con.store(num, '+FLAGS', '\\Deleted')
+                        typ, msg = con.search(None, 'All')
+                        Email = msg[0].split()
+                        print (Email)
+                except:
+                        pass
+print('Done')
 con.expunge()
-
